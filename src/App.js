97aa52b574e752b440ba467d871669cdf7faf1a0
Andrew Mager <mager@postmates.com>
2017-01-20 07:17:22 -0800

    Style

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>jordanbio.com is coming soon</h2>
-        </div>
-        <p className="App-intro">In the meantime, buy the book:</p>
-            <a href="https://www.amazon.com/Michael-Jordan-Life-Roland-Lazenby/dp/031619476X"><img src="https://images-na.ssl-images-amazon.com/images/I/51q6AsIhA1L._SX323_BO1,204,203,200_.jpg" /></a>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>jordanbio.com is coming soon</h2>
+                </div>
+                <div className="App-body">
+                    <p>In the meantime, buy the book:</p>
+                    <p><a href="https://www.amazon.com/Michael-Jordan-Life-Roland-Lazenby/dp/031619476X"><img src="https://images-na.ssl-images-amazon.com/images/I/51q6AsIhA1L._SX323_BO1,204,203,200_.jpg" alt="Michael Jordan: The Life"/></a></p>
+                </div>
+                <div className="App-footer">
+                    <p>Follow <a href="http://twitter.com/lazenby">@lazenby</a> on Twitter | &copy; Roland Lazenby</p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
